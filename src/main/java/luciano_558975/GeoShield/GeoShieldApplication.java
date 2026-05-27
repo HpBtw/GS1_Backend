f4b5a56f28d056b3481bbf92b790c377d94c951c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-27 20:55:15 -0300

    back-end first commit

---
+++ src/main/java/luciano_558975/GeoShield/GeoShieldApplication.java
@@ -0,0 +1,13 @@
+package luciano_558975.GeoShield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luciano_558975/GeoShield/GeoShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package luciano_558975.GeoShield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
